b6be23cdf32d09917eb2ac7654c21ab386ba2922
kevinyingyang <yingyuang@gmail.com>
2016-02-24 14:54:51 -0500

    Make OnMouseMove empty

--- myVTKInteractorStyle.h
+++ myVTKInteractorStyle.h
@@ -12,32 +12,32 @@ public:
 
 	virtual void OnMouseMove()
     {
-		/*int x = this->Interactor->GetEventPosition()[0];
-		int y = this->Interactor->GetEventPosition()[1];*/
+		///*int x = this->Interactor->GetEventPosition()[0];
+		//int y = this->Interactor->GetEventPosition()[1];*/
 
-		switch (this->State)
-		{
-		case VTKIS_WINDOW_LEVEL:
-		  /*this->FindPokedRenderer(x, y);
-		  this->WindowLevel();
-		  this->InvokeEvent(vtkCommand::InteractionEvent, NULL);*/
-		  break;
+		//switch (this->State)
+		//{
+		//case VTKIS_WINDOW_LEVEL:
+		//  /*this->FindPokedRenderer(x, y);
+		//  this->WindowLevel();
+		//  this->InvokeEvent(vtkCommand::InteractionEvent, NULL);*/
+		//  break;
 
-		/*case VTKIS_PICK:
-		  this->FindPokedRenderer(x, y);
-		  this->Pick();
-		  this->InvokeEvent(vtkCommand::InteractionEvent, NULL);
-		  break;
+		///*case VTKIS_PICK:
+		//  this->FindPokedRenderer(x, y);
+		//  this->Pick();
+		//  this->InvokeEvent(vtkCommand::InteractionEvent, NULL);
+		//  break;
 
-		case VTKIS_SLICE:
-		  this->FindPokedRenderer(x, y);
-		  this->Slice();
-		  this->InvokeEvent(vtkCommand::InteractionEvent, NULL);
-		  break;*/
-		}
+		//case VTKIS_SLICE:
+		//  this->FindPokedRenderer(x, y);
+		//  this->Slice();
+		//  this->InvokeEvent(vtkCommand::InteractionEvent, NULL);
+		//  break;*/
+		//}
 
-		// Call parent to handle all other states and perform additional work
-		//this->Superclass::OnMouseMove();
+		//// Call parent to handle all other states and perform additional work
+		////this->Superclass::OnMouseMove();
     }
 };
 vtkStandardNewMacro(myVTKInteractorStyle);
